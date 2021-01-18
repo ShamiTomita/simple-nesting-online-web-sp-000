440516b6a998b97c7fef4bda7614feff10e4d0eb
Shami Tomita <shamitomita@gmail.com>
2021-01-18 23:38:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-   programmer_hash[:dennis_ritchie][:languages]
+  x = programmer_hash[:dennis_ritchie][:languages]
+  x.to_s
 end
 
 def adding_matz
